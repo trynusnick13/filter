ce4df1c99a4dfc3fb7b013813c1682218ab3b363
Nikita Trynus <trynuspoc@gmail.com>
2020-12-08 21:46:05 +0200

    updated face detect filter with base image

--- filter_app/filters/face_detect_filter.py
+++ filter_app/filters/face_detect_filter.py
@@ -2,6 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 from filter_app.config_filter.config_facedetect import CASC_PATH
 from filter_app.filters.filter import ImageFilter
+from filter_app.image.base_image import Image
 import PIL
 import numpy
 
@@ -9,10 +10,10 @@ import numpy
 class FaceDetectFilter(ImageFilter):
     cascPath = CASC_PATH
 
-    def __init__(self, pillow_image, name_of_filter):
-        self.pillow_image = pillow_image
+    def __init__(self, image, name_of_filter):
+        self.image = image.pillow_image
         self.name_of_filter = name_of_filter
-        self.opencv_image = cv2.cvtColor(numpy.array(self.pillow_image), cv2.COLOR_RGB2BGR)
+        self.opencv_image = cv2.cvtColor(numpy.array(self.image), cv2.COLOR_RGB2BGR)
 
     def _detect_faces(self):
         faceCascade = cv2.CascadeClassifier(self.cascPath)
@@ -43,8 +44,8 @@ class FaceDetectFilter(ImageFilter):
 
 if __name__ == '__main__':
     pil_image = PIL.Image.open('./photos/test1.png')
-    image = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
-    a = FaceDetectFilter(pil_image, 'face detect filter')
+    base_image = Image("18 people", pil_image)
+    a = FaceDetectFilter(base_image, 'face detect filter')
     a.apply_filter()
 
     # find_face('./photos/test0.jpg')
--- filter_app/tests/test_face_detect.py
+++ filter_app/tests/test_face_detect.py
@@ -1,22 +1,25 @@
 from filter_app.filters.face_detect_filter import FaceDetectFilter
 import PIL
-
+from filter_app.image.base_image import Image
 
 def test_faces_png():
     pil_image = PIL.Image.open('./test_data/photos/test1.png')
-    testing_filter = FaceDetectFilter(pil_image, 'face detect filter')
+    base_image = Image("18 people on png", pil_image)
+    testing_filter = FaceDetectFilter(base_image, 'face detect filter')
     testing_filter._detect_faces()
     assert len(testing_filter.faces) == 18
 
 def test_faces_jpg():
     pil_image = PIL.Image.open('./test_data/photos/test0.jpg')
-    testing_filter = FaceDetectFilter(pil_image, 'face detect filter')
+    base_image = Image("1 people on jpg", pil_image)
+    testing_filter = FaceDetectFilter(base_image, 'face detect filter')
     testing_filter._detect_faces()
     assert len(testing_filter.faces) == 1
 
 def test_negative_faces():
     pil_image = PIL.Image.open('./test_data/photos/dog-test.jpg')
-    testing_filter = FaceDetectFilter(pil_image, 'face detect filter')
+    base_image = Image("dog", pil_image)
+    testing_filter = FaceDetectFilter(base_image, 'face detect filter')
     testing_filter._detect_faces()
     assert len(testing_filter.faces) == 0
 
