0e3c1777900c40da92d23bab37370f1fffc384b4
lyyubava <lyyubava@gmail.com>
2020-12-13 04:38:56 +0200

    change contrast

---
+++ filter_app/filters/change_contrast.py
@@ -0,0 +1,26 @@
+from PIL import Image,ImageEnhance
+import PIL
+from filter_app.filters.filter import ImageFilter
+from filter_app.image.base_image import Image
+
+class Contrast(ImageFilter):
+    def __init__(self, image, name_of_filter):
+        self.image = image.pillow_image
+        self.name_of_filter = name_of_filter
+
+    def contrast(self):
+        enhancer = ImageEnhance.Contrast(self.image)
+        factor = int(input("enter a factor "))
+        im_convert = enhancer.enhance(factor)
+        PIL.Image._show(im_convert)
+
+    def apply_filter(self):
+        self.contrast()
+
+if __name__ == '__main__':
+    img = PIL.Image.open("./photos/test4.png")
+    base_image = Image("contrast photo", img)
+    test_image = Contrast(base_image, "change contrast")
+    test_image.apply_filter()
+
+
