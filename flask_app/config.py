96e39ba78a9efd197fa5abd6c23002f70e15c228
Nuznhy <73580694+Nuznhy@users.noreply.github.com>
2020-12-27 18:56:51 +0200

    upload style fixed - TODO: fix flask upload

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,5 +1,5 @@
 class Configuration:
     DEBUG = True
-    UPLOAD_FOLDER = '/Users/nicktrynus/Projects/filter/flask_app/static'
+    UPLOAD_FOLDER = 'C://Users/Nikitos/filter/flask_app/static'
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
